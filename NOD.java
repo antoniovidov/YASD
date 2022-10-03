7ed3221f1e2bf082d86e989670eedd7de1df693e
antoniovidov <91030382+antoniovidov@users.noreply.github.com>
2022-10-03 09:33:58 +0300

    Create NOD.java

---
+++ NOD.java
@@ -0,0 +1,21 @@
+package YASD;
+
+import java.util.Scanner;
+
+public class NOD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a : ");
+        int a = scanner.nextInt();
+        System.out.println("Input b : ");
+        int b = scanner.nextInt();
+
+        if(a>b){
+            a = a-b;
+        }
+        else if(b>a){
+            b = b-a;
+        }
+        System.out.println("N O D e : " + a + b);
+    }
+}
